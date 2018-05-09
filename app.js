63ef4bf198c2ca26a99556c257de637c96e5e50a
John McKillip <jmckillip@wsol.com>
2018-05-09 10:06:53 -0500

    Fixed issue with results in ablum search.

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.post('/album', function (req, res) {
             }
             spotifyApi.searchTracks(query)
                 .then(function (data) {
-                    var results = data.body.tracks.items;
+                    var results = data.body.albums.items;
                     if (results.length === 0) {
                         return slack(res, 'Could not find that album.');
                     }
